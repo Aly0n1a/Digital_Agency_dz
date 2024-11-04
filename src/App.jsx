4ed2ade1c6627a6a87c04caf29159ef9773461f9
Чмирёва Алёна <meloman.ytxd@gmail.com>
2024-11-04 16:33:21 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.jsx'
+import Header from './components/Header/Header.jsx'
+import Hero from './components/Hero/Hero.jsx'
+import Zoom from './components/Zoom/Zoom.jsx'
+import System from './components/System/System.jsx'
+import Plagin from './components/Plagin/Plagin.jsx'
+import Cards from './components/Cards/Cards.jsx'
+import Part from './components/Part/Part.jsx'
+import Footer from './components/Footer/Footer.jsx'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Zoom/>
+    <System/>
+    <Plagin/>
+    <Cards/>
+    <Part/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
